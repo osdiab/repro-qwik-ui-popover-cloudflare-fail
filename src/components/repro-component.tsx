a3039ba980661083614ae19318b48e4bc5d99e35
Omar Diab <me@omardiab.com>
2024-06-24 15:55:10 -0400

    add repro

---
+++ src/components/repro-component.tsx
@@ -0,0 +1,28 @@
+import { component$, useSignal } from '@builder.io/qwik';
+import { Popover, usePopover } from '@qwik-ui/headless';
+export const ReproComponent = component$(() => {
+  const popoverId = 'programmatic-id';
+  const anchorRef = useSignal<HTMLElement | undefined>();
+  const { togglePopover } = usePopover(popoverId);
+
+  return (
+    <Popover.Root id={popoverId} bind:anchor={anchorRef} manual>
+      {/* can be anywhere as long as ref is set */}
+      <button
+        ref={anchorRef}
+        class="popover-invoker"
+        preventdefault:click
+        onKeyDown$={async (e) => {
+          if (e.key === 'o') {
+            await togglePopover();
+          }
+        }}
+      >
+        Focus me and press the 'o' key!
+      </button>
+      <Popover.Panel class="popover-panel popover-programmatic">
+        I'm a programmatically opened popover!
+      </Popover.Panel>
+    </Popover.Root>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { ReproComponent } from "~/components/repro-component";
 
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      <ReproComponent />
     </>
   );
 });
